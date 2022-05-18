911c9ffafeb827d3b8906ea644e6aa9c8c72eed2
huangjunkang <2661378467@qq.com>
2022-05-18 16:58:01 +0800

    ·

--- src/App.svelte
+++ src/App.svelte
@@ -8,16 +8,10 @@
   <h1>Hello world!</h1>
 
   <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Counter />
+  <Counter />
+  <Counter />
+  
 </main>
 
 <style>
